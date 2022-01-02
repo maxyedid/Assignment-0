764738d577697ec8401ec87210f824dff318abf9
Max Yedid <maxyedid1@gmail.com>
2022-01-02 05:34:06 -0500

    Added answer using MIN_VALUE and MAX_VALUE

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,16 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < min) {
+      min = nums[i]
+    }
+    if (nums[i] > max) {
+      max = nums[i]
+    }
+  }
+  return max + min
 }
 
 // Do not edit this line;
