b6a94fd83a9c7b0252b74ea9d717b592875490cf
Max Yedid <maxyedid1@gmail.com>
2022-01-02 06:38:01 -0500

    Added Answer to Pair Sum
    Honestly, I kind of rushed this one and used a nested loop for it, making it O(n^2) but it was all I could really think of for an unsorted list

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,16 @@
 function pairSum(nums, target) {
   // Insert code here;
+   if (nums.length < 2) {
+  throw "Array should not be less than 2 elements"
+  }
+  for (let i = 0; i < nums.length - 1; i ++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;
