f0b898a8b2c9b97d708d48878fb6808eddcf53f8
Max Yedid <maxyedid1@gmail.com>
2022-01-02 06:07:24 -0500

    Added answer, looks a bit complicated and uunorganized

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,10 @@
 function frequencyCounter(word) {
   // Insert code here;
+   let answer =  {}
+  for (let i = 0; i < word.length; i++) {
+    answer[word[i]] = answer[word[i]] ? answer[word[i]] + 1: 1
+  }
+  return answer
 }
 
 // Do not edit this line;
