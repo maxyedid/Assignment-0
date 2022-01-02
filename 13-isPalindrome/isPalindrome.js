8edc2ab9fd9e05366e26013deb8a68afcd6b1074
Max Yedid <maxyedid1@gmail.com>
2022-01-02 05:49:44 -0500

    Added answer to isPalindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
   // Insert code here;
+  for (let i = 0; i < word.length/2; i++) {
+    if (word[i] != word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // Do not edit this line;
