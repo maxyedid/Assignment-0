0733395f59584df52cacd3854059ba6cd567d9cd
Max Yedid <maxyedid1@gmail.com>
2022-01-02 05:07:39 -0500

    Answer to title casing edit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,15 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let result = ""
+  result += title[0].toUpperCase()
+  for (let i = 0; i < title.length - 1; i++) {
+    if (title[i] === " ") {
+      result += title[i+1].toUpperCase()
+      continue;
+    }
+    result += title[i+1]
+  }
+  return result;
 }
 
 // Do not edit this line;
