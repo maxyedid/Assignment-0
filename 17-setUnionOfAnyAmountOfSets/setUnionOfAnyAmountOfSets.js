7164e7701c3f4c1a81c84f52bcb8eec420dd11d0
Max Yedid <maxyedid1@gmail.com>
2022-01-02 06:31:13 -0500

    Added Answer to union of sets

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,12 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let resultingSet = new Set()
+  for (let i = 0; i < args.length; i++) {
+    for (let element of args[i]) {
+      resultingSet.add(element)
+    }
+  }
+  return resultingSet;
 }
 
 // Do not edit this line;
