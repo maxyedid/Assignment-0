09896dceba11f404629a79a76d01932e56b187b5
Max Yedid <maxyedid1@gmail.com>
2022-01-02 07:07:43 -0500

    Made Binary Search algorithm
    It was a bit harder to work through for me because usually, my binary search methods have a start and end parameters but this one didn't so I tried to use a while loop, but it wasn't allowed since it was non-recursive. Then, I tried to change the array and half its length everytime I call the method again, but I wasn't allowed to mutate the array, so I ended up using the slice method, which returns a new subarray from a certain start and end

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,17 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    if (nums.length <= 1 && nums[0] != target) {
+      return false;
+    }
+    let middle = Math.floor(nums.length / 2);
+    if (nums[middle] == target) {
+      return true;
+    } else if (nums[middle] < target) {
+      return this.binarySearch(nums.slice(middle + 1), target)
+    } else {
+      return this.binarySearch(nums.slice(0, middle), target)
+    }
   }
 }
 
